17911fbda4c9c8bbf4904defd74a1be02bb7dd62
evolvesofttech <sameerthite47@gmail.com>
2020-05-23 13:40:56 +0530

    Added all task routes

--- src/routes/task.js
+++ src/routes/task.js
@@ -3,6 +3,81 @@ const router = new express.Router();
 const Task = require('../models/task');
 const auth = require('../middleware/auth');
 
+//Add new task
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    });
+    try {
+        await task.save();
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
 
+//Get all tasks of logged in user
+router.get('/tasks', auth, async (req, res) => {
+    try {
+        await req.user.populate('tasks').execPopulate();
+        res.status(200).send(req.user.tasks);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+//Get signle task
+router.get('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+//Update task
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowUpdates = ["description", "completed"];
+    const isValidOperation = updates.every(update => allowUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates!" })
+    }
+
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
+
+        if (!task) {
+            return res.status(404).send({ error: 'Task not found.' });
+        }
+        updates.forEach(update => task[update] = req.body[update]);
+
+        await task.save();
+
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+//Delete task
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
+
+        if (!task) {
+            return res.status(404).send({ error: 'Task not found.' })
+        }
+        res.status(200).send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
 
 module.exports = router;
